2809ddb55f3157372c569e582132c00feef93ae1
Emma Freeman <freemoe04@gmail.com>
2017-11-28 23:48:54 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,7 @@
-# I call on the method, say_hello, and give it the string "Gabriela" 
-say_hello("Gabriela")
-
-# The method prints this text to the screen:
-Hello Gabriela!
+def say_hello(name)
+  puts "Hello Gabriela"
+end
+ 
+def say_hello(name="Ruby Programmer!")
+   puts ("Hello #{name}!")
+end
